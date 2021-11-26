236e540fa641df0c3c797ee6f3a0490e773905cc
João <santoscostaj808@gmail.com>
2021-11-26 18:07:45 -0300

    pratica2

---
+++ fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-01-JavaScript-Primeiros-passos/pratica2.js
@@ -0,0 +1,19 @@
+let patientId = 50;
+patientId = "50"
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+const base = 5;
+let height = 8;
+const area = base * height;
+var perimeter = "5, 8, 40"; 
+
+console.log(base)
+console.log(height)
+console.log(area)
+console.log(perimeter)
+console.log(typeof patientAge)
+console.log(typeof patientId)
