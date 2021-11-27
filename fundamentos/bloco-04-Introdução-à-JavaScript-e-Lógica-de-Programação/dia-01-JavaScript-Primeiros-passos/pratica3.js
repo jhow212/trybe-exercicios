1763510d8ad4cadc9e5b33a42cd37be75a126db3
João <santoscostaj808@gmail.com>
2021-11-26 23:33:04 -0300

    pratica3

---
+++ fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-01-JavaScript-Primeiros-passos/pratica3.js
@@ -0,0 +1,11 @@
+const pessoaCandidataNota = 90;
+
+if (pessoaCandidataNota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!")
+}
+else if (pessoaCandidataNota < 80 && pessoaCandidataNota >= 60) {
+    console.log("Você está na nossa lista de espera")
+}
+else {
+    console.log("Você foi reprovada(o)")
+}
