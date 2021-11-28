399ba5d13b82a2993f61d1822d41156380b61e8d
João <santoscostaj808@gmail.com>
2021-11-28 16:54:55 -0300

    adiciona primeira parte  dos exercicos

--- fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-01-JavaScript-Primeiros-passos/exercicios.js
+++ fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-01-JavaScript-Primeiros-passos/exercicios.js
@@ -2,3 +2,14 @@ const a = 6;
 const b = 19;
 
 let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
+
