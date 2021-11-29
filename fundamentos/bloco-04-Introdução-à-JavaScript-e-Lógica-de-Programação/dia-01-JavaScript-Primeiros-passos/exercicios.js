5539b80564320782191b837fc51ed16c085b58d1
João <santoscostaj808@gmail.com>
2021-11-29 13:10:41 -0300

    corrigindo quinta parte dos exercicos

--- fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-01-JavaScript-Primeiros-passos/exercicios.js
+++ fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-01-JavaScript-Primeiros-passos/exercicios.js
@@ -60,9 +60,9 @@ const angulo3 = 20;
 if (angulo1 + angulo2 + angulo3 === 180) {
     console.log(true)
 }
-else if (angulo1 + angulo2 + angulo3 !== 180) {
-    console.log(false)
+else if (angulo1 < 0 || angulo2 < 0 || angulo3 < 0 ) {
+    console.log("Erro")
 }
 else {
-    console.log("Erro")
+    console.log(false)
 }
