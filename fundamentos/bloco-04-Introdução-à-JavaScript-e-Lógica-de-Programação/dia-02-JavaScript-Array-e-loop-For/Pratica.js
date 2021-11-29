8769ddc098ae6effda9da3f755a6519ebcc00c5e
João <santoscostaj808@gmail.com>
2021-11-29 18:36:36 -0300

    pratica exercicios 1, 2, 3 e 4

---
+++ fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-02-JavaScript-Array-e-loop-For/Pratica.js
@@ -0,0 +1,18 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
+console.log(numbers[5]);
+console.log(numbers[6]);
+console.log(numbers[7]);
+console.log(numbers[8]);
+console.log(numbers[9]);
+
+let soma = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+
+let divisao = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9] / 10;
+console.log(soma);
+console.log(divisao);
