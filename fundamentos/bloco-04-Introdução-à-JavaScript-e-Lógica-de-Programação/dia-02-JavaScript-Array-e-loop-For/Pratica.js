ec97c9feb7940ed4c7919279406d5d6a1ce78898
João <santoscostaj808@gmail.com>
2021-11-30 12:04:44 -0300

    pratica exercicio 4 e 5 revisadas

--- fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-02-JavaScript-Array-e-loop-For/Pratica.js
+++ fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-02-JavaScript-Array-e-loop-For/Pratica.js
@@ -1,33 +1,38 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// console.log(numbers[0]);
-// console.log(numbers[1]);
-// console.log(numbers[2]);
-// console.log(numbers[3]);
-// console.log(numbers[4]);
-// console.log(numbers[5]);
-// console.log(numbers[6]);
-// console.log(numbers[7]);
-// console.log(numbers[8]);
-// console.log(numbers[9]);
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
+console.log(numbers[5]);
+console.log(numbers[6]);
+console.log(numbers[7]);
+console.log(numbers[8]);
+console.log(numbers[9]);
 
-// let soma = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+let soma = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
 
-// let divisao = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9] / 10;
-// console.log(soma);
-// console.log(divisao);
+let divisao = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9] / 10;
+console.log(soma);
+console.log(divisao);
+
+if (divisao > 20) {
+  console.log("valor maior que 20");
+}
+else {
+  console.log("valor menor ou igual a 20");
+}
+
 
 let resultado = 0;
+let maiorNumero = 0;
 
-for(let maior = 0; maior < numbers.length; maior +=1) {
-  let numero = numbers[maior];
-    // console.log("comparativo:", numero);
-        
-for(let numeroG = 0; numeroG < numbers.length; numeroG +=1) {
-  let numeros = numbers[numeroG];
-    //  console.log("escolhas:", numeros);
-    
+for(let index = 0; index < numbers.length; index +=1) {
+   resultado = numbers[index];
+
+  if (resultado > maiorNumero) {
+    maiorNumero = resultado;
   }
 }
-resultado = Math.max(numbers);
-console.log(resultado);
+console.log(maiorNumero);
