e98ed9282719330441520d25d9c0b5d370306cf2
João <santoscostaj808@gmail.com>
2021-12-02 13:35:33 -0300

    Pratica parte2 exercicio2 funçao

--- fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-04-JavaScript-Objetos-e-funções/praticaParte2função.js
+++ fundamentos/bloco-04-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-04-JavaScript-Objetos-e-funções/praticaParte2função.js
@@ -11,5 +11,22 @@ function verificandoPalindromo(palavra) {
 console.log(verificandoPalindromo("arara"));
 console.log(verificandoPalindromo("joao"));
 
+//Exercicio 2
+let teste = [2, 4, 6, 7, 10, 0, -3];
 
- 
+function verificandoMaiorIndice(array) {
+  let resultado = 0;
+  let maiorNumero = 0;
+
+  for (let index = 0; index < array.length; index += 1) {
+    resultado = array[index];
+
+    if (resultado > maiorNumero) {
+      maiorNumero = resultado;
+    }
+  }
+  let indice = array.indexOf(maiorNumero);
+  return indice;
+}
+
+console.log(verificandoMaiorIndice(teste));
