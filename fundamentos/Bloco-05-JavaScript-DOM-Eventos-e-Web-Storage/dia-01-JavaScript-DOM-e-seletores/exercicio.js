528ee1ced3506e9fc1944313ed57ce975af7fdd2
João <santoscostaj808@gmail.com>
2021-12-08 19:27:45 -0300

    exercicios finalizados

--- fundamentos/Bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-01-JavaScript-DOM-e-seletores/exercicio.js
+++ fundamentos/Bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-01-JavaScript-DOM-e-seletores/exercicio.js
@@ -6,4 +6,15 @@ document.getElementsByClassName('center-content')[0].style.background = 'White'
 
 document.getElementsByClassName('title')[0].innerText = 'Exercício 5.1 - JavaScript'
 
-document.getElementsByTagName('p').innerText.style = 'strong'
+let tag = document.getElementsByTagName('p')
+for (let i = 0; i < tag.length; i += 1) {
+   let upperCase = tag[i].innerText.toUpperCase()
+    tag[i].innerText = upperCase;
+    
+}
+
+let tagTexto = document.getElementsByTagName('p')
+
+for (let i = 0; i < tagTexto.length; i += 1) {
+    console.log(tagTexto[i].innerText);
+}
