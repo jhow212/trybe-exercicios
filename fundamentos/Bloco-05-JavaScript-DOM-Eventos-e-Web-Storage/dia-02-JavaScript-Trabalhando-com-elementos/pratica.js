e48dba07eb29679802873e9bfebb97df25b5b8f5
João <santoscostaj808@gmail.com>
2021-12-09 00:36:03 -0300

    praticas finalizados

--- fundamentos/Bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-02-JavaScript-Trabalhando-com-elementos/pratica.js
+++ fundamentos/Bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-02-JavaScript-Trabalhando-com-elementos/pratica.js
@@ -13,7 +13,7 @@ ondeVoceEsta.previousElementSibling
 ondeVoceEsta.nextSibling
 ondeVoceEsta.nextElementSibling
 
-let terceiroFPai = pai.lastElementChild.previousElementSibling
+let terceiroFPai = pai.firstChild.nextElementSibling.nextElementSibling
 
 console.log(pai.firstElementChild)
 console.log(ondeVoceEsta.previousElementSibling)
@@ -21,3 +21,31 @@ console.log(ondeVoceEsta.nextSibling)
 console.log(ondeVoceEsta.nextElementSibling)
 console.log(terceiroFPai)
 
+
+let irmaoMaisNovo = document.createElement('section');
+pai.appendChild(irmaoMaisNovo);
+irmaoMaisNovo.id = 'irmaoMaisNovo';
+
+let filhoDeOndeVcEsta = document.createElement('section')
+ondeVoceEsta.appendChild(filhoDeOndeVcEsta);
+filhoDeOndeVcEsta.id = 'terceiroFilhoDoFilho';
+
+let primeiroFilhoDoPrimeirofilhoDoFilho = document.createElement('section')
+primeiroF.appendChild(primeiroFilhoDoPrimeirofilhoDoFilho)
+primeiroFilhoDoPrimeirofilhoDoFilho.id = 'primeiroFilhoDoPrimeirofilhoDoFilho'
+
+let voltandoEAvancandoParaTerceiroFilho = primeiroFilhoDoPrimeirofilhoDoFilho.parentElement.parentElement.parentElement.firstChild.nextElementSibling.nextElementSibling.nextElementSibling;
+
+function remover (idDoPai, idDoFilho) {
+    idDoPai.removeChild(idDoFilho)
+}
+remover(pai, irmaoMaisNovo)
+remover(pai, primeiroFilho)
+remover(elementoOndeVoceEsta, segundoEUltimoFilhoDoFilho)
+remover(elementoOndeVoceEsta, terceiroFilhoDoFilho)
+remover(pai, terceiroFilho)
+remover(pai, quartoEUltimoFilho)
+
+
+console.log(document.getElementById('pai'));
+console.log(document.getElementById('paiDoPai'));
