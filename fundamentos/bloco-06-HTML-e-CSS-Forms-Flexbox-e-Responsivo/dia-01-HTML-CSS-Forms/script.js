dca16fab031352af6e8ed7e17198862aadbfe50d
João <santoscostaj808@gmail.com>
2022-01-09 20:04:35 -0300

    exercicios ainda nn finalizados

--- fundamentos/bloco-06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-01-HTML-CSS-Forms/script.js
+++ fundamentos/bloco-06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-01-HTML-CSS-Forms/script.js
@@ -1,7 +1,115 @@
-const estados = document.getElementById('seuestado');
-const todos = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás ','Maranhão', 'Mato Grosso', 'Mato Grosso do Sul ', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte',
-    'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
+// const JustValidate = require('just-validate')
+
+// const validate = new JustValidate('#teste');
+const validate = new window.JustValidate('#teste');
+validate.addField('#comeco', [
+  {
+    rule: 'minLength',
+    value: 3,
+
+  },
+  {
+    rule: 'maxLength',
+    value: 40,
+  },
+  {
+    rule: 'required',
+    errorMessage: 'Name is required',
+  }
+])
+  .addField('#seuemail', [
+    {
+      rule: 'required',
+      errorMessage: 'Email is required',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Email is invalid!',
+    },
+    {
+      rule: 'maxLength',
+      value: 50,
+    }
+  ])
+  .addField('#seucpf', [
+    {
+      rule: 'maxLength',
+      value: 11,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'CPF is required',
+    }
+  ])
+  .addField('#seuendereco', [
+    {
+      rule: 'maxLength',
+      value: 200,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Endereço is required',
+    }
+  ])
+  .addField('#suacidade', [
+    {
+      rule: 'maxLength',
+      value: 28,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Cidade is required',
+    }
+  ])
+  .addField('#seuestado', [
+    {
+      rule: 'required',
+      errorMessage: 'Estado is required',
+    }
+  ])
+  .addField('#tipo', [
+    {
+      rule: 'required',
+      errorMessage: 'Moradia is required',
+    }
+  ])
+  .addField('#resumo', [
+    {
+      rule: 'maxLength',
+      value: 1000,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Resumo is required'
+    }
+  ])
+  .addField('#empregado', [
+    {
+      rule: 'maxLength',
+      value: 40,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Cargo is required'
+    }
+  ])
+  .addField('#areadoemprego', [
+    {
+      rule: 'maxLength',
+      value: 500,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Descrição is required'
+    }
+  ]);
+
+document.getElementById('inicio').DatePickerX.init();
+
 const teste = document.getElementById('teste');
+
+const estados = document.getElementById('seuestado');
+const todos = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás ', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul ', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
 
 
 
@@ -10,16 +118,16 @@ console.log(estados);
 
 let cadaUm = '';
 
-for (let i = 0; i < todos.length; i +=1) {
+for (let i = 0; i < todos.length; i += 1) {
   cadaUm = todos[i];
   let criando = document.createElement('Option');
-  criando.innerHTML = cadaUm 
+  criando.innerHTML = cadaUm
   estados.appendChild(criando);
 }
 
 const arrumando = document.getElementById('botao');
 
-function salvando (event) {
+function salvando(event) {
   event.preventDefault()
   let nova = document.createElement('div');
   nova.id = 'dados'
@@ -29,34 +137,8 @@ function salvando (event) {
 }
 
 arrumando.addEventListener('click', salvando)
-function names () {
-  let praca = document.getElementById('dados')
-  const nomez = document.getElementById('comeco').value;
-  praca.innerHTML = nomez
-  const emails = document.getElementById('seuemail').value;
-  const cpfs = document.getElementById('seucpf').value
-  // const enderecos = document.getElementById('seuendereco').value
-  // const cidades = document.getElementById('suacidade').value
-  // const estadual = document.getElementById('seuestado').value
-  // const casas = document.getElementById('tipo').value
-  // const predios = document.getElementById('tipos').value
-  // const resumos = document.getElementById('resumo').value
-  // const cargos = document.getElementById('empregado').value
-  // const descricoes = document.getElementById('areadoemprego').value
-  // const datas = document.getElementById('inicio').value
-  
-  praca.innerHTML = emails
-  praca.innerHTML = cpfs
-  // praca.innerHTML = enderecos
-  // praca.innerHTML = cidades
-  // praca.innerHTML = estadual
-  // praca.innerHTML = casas
-  // praca.innerHTML = predios
-  // praca.innerHTML = resumos
-  // praca.innerHTML = cargos
-  // praca.innerHTML = descricoes
-  // praca.innerHTML = datas
-  
-}
+
+
+
 arrumando.addEventListener('click', names)
 console.log(arrumando)
