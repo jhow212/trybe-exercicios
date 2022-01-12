2bdbd9ddd87059981ab7ca6ac194c6264b4e71ae
João <santoscostaj808@gmail.com>
2022-01-12 19:35:43 -0300

    Exercicios 1 e 2 parte 2

---
+++ fundamentos/bloco-07-Introdução-à-JavaScript-ES6-e-Testes-Unitários/dia-01-JavaScript-ES6-let-const-arrow-functions-e-template-literals/exercicioParte2.js
@@ -0,0 +1,31 @@
+const fatorial = number => {
+    let resultado = 1;
+
+    for (let i = 2; i <= number; i +=1) {
+        resultado *= i;
+    }
+    return resultado;
+}
+
+console.log(fatorial(3));
+
+//EXERCICIO 2 
+
+const maiorPalavra = frase => {
+    let arrayPalavra = frase.split(' ');
+    let maiorPalavra = 0;
+    let resultado = '';
+
+    for (const palavra of arrayPalavra) {
+        if (palavra.length > maiorPalavra) {
+            maiorPalavra = palavra.length;
+            resultado = palavra;
+        }
+    }
+    return resultado;
+}
+
+console.log(maiorPalavra("to ansioso para hoje e para dia 21!"));
+
+//EXERCICIO 3
+
