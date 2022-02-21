34baf576599bf2c65bf4f561c8d36b770f9e650d
João <santoscostaj808@gmail.com>
2022-02-21 18:52:04 -0300

    Para Fixar

--- front-end/Bloco-11-Componentes-com-Estado-Eventos-e-Formularios-com-React/dia-01-Componentes-com-estado-e-eventos/paraFixar-dia-02/src/App.js
+++ front-end/Bloco-11-Componentes-com-Estado-Eventos-e-Formularios-com-React/dia-01-Componentes-com-estado-e-eventos/paraFixar-dia-02/src/App.js
@@ -75,35 +75,57 @@ import React from 'react';
      this.botão = this.botão.bind(this)
      this.botão2 = this.botão2.bind(this)
      this.botão3 = this.botão3.bind(this)
+     this.mudarDeCor = this.mudarDeCor.bind(this)
+     this.mudarDeCor2 = this.mudarDeCor2.bind(this)
+     this.mudarDeCor3 = this.mudarDeCor3.bind(this)
    }
 
     botão() {
     console.log('cliquei');
-    this.setState((estadoAnterior, _props) => ({
+    this.setState((estadoAnterior) => ({
       numeroDeCliquei: estadoAnterior.numeroDeCliquei + 1
-    }))
+    }), () => {
+      console.log(`Botão1 ${this.mudarDeCor(this.state.numeroDeCliquei)}`);
+    })
   } 
 
    botão2() {
     console.log('Não cliquei');
-    this.setState((estadoAnterior, _props) => ({
+    this.setState((estadoAnterior) => ({
       numeroDeNao: estadoAnterior.numeroDeNao + 1
-    }))
+    }), () => {
+      console.log(`Botão2 ${this.mudarDeCor2(this.state.numeroDeNao)}`);
+    })
   }
 
    botão3() {
     console.log('Talvez cliquei');
-    this.setState((estadoAnterior, _props) => ({
-      numeroDeTalvez: estadoAnterior.numeroDeTalvez + 1
-    }))
+    this.setState((estadoAnterior) => ({
+      numeroDeTalvez: estadoAnterior.numeroDeTalvez + 1 
+    }), () => {
+      console.log(`Botão3 ${this.mudarDeCor3(this.state.numeroDeTalvez)}`);
+    })
+  }
+
+  mudarDeCor(num) {
+    return num % 2 === 0 ? 'green' : 'red';
+  }
+
+  mudarDeCor2(num) {
+    return num % 2 === 0 ? 'blue' : 'purple';
+  }
+
+  mudarDeCor3(num) {
+    return num % 2 === 0 ? 'yellow' : 'violet';
   }
 
    render() {
+     const { numeroDeCliquei, numeroDeNao, numeroDeTalvez } = this.state;
      return (
        <div>
-         <button onClick={this.botão}>Eu Cliquei {this.state.numeroDeCliquei}!</button>
-         <button onClick={this.botão2}>Não Cliquei {this.state.numeroDeNao}!!</button>
-         <button onClick={this.botão3}>Talvez Cliqeui {this.state.numeroDeTalvez}!?</button>
+         <button onClick={this.botão } style={{backgroundColor: this.mudarDeCor(numeroDeCliquei)}}>Eu Cliquei {numeroDeCliquei}!</button>
+         <button onClick={this.botão2} style={{backgroundColor: this.mudarDeCor2(numeroDeNao)}}>Não Cliquei {numeroDeNao}!!</button>
+         <button onClick={this.botão3} style={{backgroundColor: this.mudarDeCor3(numeroDeTalvez)}}>Talvez Cliqeui {numeroDeTalvez}!?</button>
        </div>
      )
    }
